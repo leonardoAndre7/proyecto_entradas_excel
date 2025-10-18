66b28a2cc836b473d3f6868d106fbb17a7762c43
LeonardoAndre7 <leo283441@gmail.com>
2025-10-18 09:37:11 -0500

    configracion postgresql render

--- webcliente/settings.py
+++ webcliente/settings.py
@@ -120,10 +120,11 @@ WSGI_APPLICATION = 'webcliente.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+    'default': dj_database_url.config(
+        default='postgresql://db_evento_sf84_user:jL4o93bjx3cIcPZLNnc8etTnMw28wlW2@dpg-d3i0eu1r0fns73coedpg-a.oregon-postgres.render.com/db_evento_sf84',
+        conn_max_age=600
+    )
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

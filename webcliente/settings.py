fd317ac0b5f2f8f017d08caec1c3b478781fb59c
LeonardoAndre7 <leo283441@gmail.com>
2025-10-18 09:24:07 -0500

    Eliminar credenciales SMTP del código

--- webcliente/settings.py
+++ webcliente/settings.py
@@ -15,7 +15,7 @@ import os
 from django.conf import settings
 import socket
 import dj_database_url
-
+from decouple import config
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,14 +34,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-
 #CORREO DE DONDE SE ENVIARAN LOS QR, Y BIENVENIDAD
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "u00024284@sise.com.pe"
-EMAIL_HOST_PASSWORD = "zzwo asxa pxlk knpe"  # 🔹 clave de aplicación de Gmail
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 

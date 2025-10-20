9f8ef9973839102021dd7146b95f623e82c428d6
LeonardoAndre7 <leo283441@gmail.com>
2025-10-20 13:14:48 -0500

    Actualizar manage.py y agregar requirements.txt para deploy

--- webcliente/manage.py
+++ webcliente/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webcliente.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webcliente.webcliente.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

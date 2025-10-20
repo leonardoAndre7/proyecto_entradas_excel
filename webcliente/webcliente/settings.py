04a90ef24f3efc9a0404498c00df3a31b579279d
LeonardoAndre7 <leo283441@gmail.com>
2025-10-20 15:31:13 -0500

    desploy ultzz

--- webcliente/webcliente/settings.py
+++ webcliente/webcliente/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', 'clave-de-desarrollo-temporal')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [

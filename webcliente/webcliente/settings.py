1ef3f4b2c9778a72813be6a7836cb89f4f037788
LeonardoAndre7 <leo283441@gmail.com>
2025-10-23 10:26:54 -0500

    cambia host de correo

--- webcliente/webcliente/settings.py
+++ webcliente/webcliente/settings.py
@@ -109,7 +109,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # --- EMAIL ---
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')

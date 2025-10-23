6ee4259c9c6e21e884e2273bde32ecc30c4aaa11
LeonardoAndre7 <leo283441@gmail.com>
2025-10-23 11:18:31 -0500

    imprime el correo en la consola

--- webcliente/webcliente/settings.py
+++ webcliente/webcliente/settings.py
@@ -108,7 +108,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # --- EMAIL ---
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True

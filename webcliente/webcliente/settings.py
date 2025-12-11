31123dd2d33cf13ce38b860c3a0b349f8b7870de
LeonardoAndre7 <leo283441@gmail.com>
2025-12-10 19:51:17 -0500

    co

--- webcliente/webcliente/settings.py
+++ webcliente/webcliente/settings.py
@@ -117,9 +117,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL1")
 
 
 

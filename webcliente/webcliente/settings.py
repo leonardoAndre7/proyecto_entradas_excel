b33d47fdd01c4640e7f83f753424d667991d84c9
LeonardoAndre7 <leo283441@gmail.com>
2025-12-12 17:23:35 -0500

    user

--- webcliente/webcliente/settings.py
+++ webcliente/webcliente/settings.py
@@ -117,7 +117,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config("BREVO_SMTP_KEY")
+EMAIL_HOST_USER = config("DEFAULT_FROM_EMAIL1")
 EMAIL_HOST_PASSWORD = config("BREVO_SMTP_KEY")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL1")
 

5e0c205d09411a034ff8ef21c9efb2ee329625fd
LeonardoAndre7 <leo283441@gmail.com>
2025-12-12 18:41:31 -0500

    user3

--- webcliente/webcliente/settings.py
+++ webcliente/webcliente/settings.py
@@ -117,7 +117,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config("BREVO_SMTP_USER")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER1")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD1")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL1")
 
